eee7b1fe2c1151d734f1fc1ffd94080e3e212f87
shady97 <iyer.sr@husky.neu.edu>
2020-08-22 12:40:15 -0400

    Updates

---
+++ precache-manifest.7d2acdf1cf7f79559391a089e6a9bd4f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3cf3048f3178612387e2448e719f235",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dd0ad56a7387f6c7fdf9",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "c85e1d6933834108bcca",
+    "url": "/portfolio/static/css/main.0147be94.chunk.css"
+  },
+  {
+    "revision": "dd0ad56a7387f6c7fdf9",
+    "url": "/portfolio/static/js/2.7c7cd560.chunk.js"
+  },
+  {
+    "revision": "c1ed0d2e13b355dc00e44860c1d009ef",
+    "url": "/portfolio/static/js/2.7c7cd560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c85e1d6933834108bcca",
+    "url": "/portfolio/static/js/main.97a90a35.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "7c20d2acf2ecccd8b64e2a18331b0772",
+    "url": "/portfolio/static/media/circle-cropped.7c20d2ac.png"
+  },
+  {
+    "revision": "51ba4e0176f99ca3e0dc6e57ad459a8f",
+    "url": "/portfolio/static/media/resume.51ba4e01.png"
+  }
+]);
